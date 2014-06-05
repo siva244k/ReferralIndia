8074b92185b5832880ecaf12bc590788fe1d9d61
siva244k <siva244k@gmail.com>
2014-06-05 19:39:07 +0530

    log4j,slf4j configured

--- src/main/java/com/ric/web/controller/ReferralJobController.java
+++ src/main/java/com/ric/web/controller/ReferralJobController.java
@@ -1,5 +1,6 @@
 package com.ric.web.controller;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import com.ric.web.model.PostRequest;
 
 @RestController
 public class ReferralJobController {
+	static Logger log = LoggerFactory.getLogger(ReferralJobController.class);
 
 	@Autowired
 	private IReferralJobService service;
@@ -27,28 +29,31 @@ public class ReferralJobController {
 
 	@RequestMapping(value = "post", method = RequestMethod.POST)
 	public PostResponse post(final PostRequest request) {
-		
-		System.out.println("request payload:" + request);
-		
+
+		log.info("request payload: skill={},position={},expto={}",
+				request.getSkill(), request.getPosition(), request.getExpTo());
+
 		PostResponse response = new PostResponse();
 		ReferralJob rj = new ReferralJob();
 		rj.setSkill(request.getSkill());
 		rj.setPosition(request.getPosition());
 		rj.setExpTo(request.getExpTo());
 		try {
-		  service.create(rj);
-		  response.setStatus("SUCCESS");
-		  response.setStatusMessage("Job got posted successfully");
-		} catch(Exception e) {
-			System.out.println("Error cause:"+e.getCause() + ", Error message:"+e.getMessage());
-		} 
-		
+			service.create(rj);
+			response.setStatus("SUCCESS");
+			response.setStatusMessage("Job got posted successfully");
+		} catch (Exception e) {
+			log.error("Error cause:{} Error message:{}", e.getCause(),
+					e.getMessage());
+		}
+
 		return response;
 	}
-	
+
 	@RequestMapping("home")
 	public ModelAndView getHomePage() {
 		ModelAndView mv = new ModelAndView();
+		log.info("post page is going to launch ");
 		mv.setViewName("layout/my-layout");
 		return mv;
 	}
